bd965f88a1882e4097681b81880c456efe989635
Christopher Miller <miller1997@gmail.com>
2020-12-13 06:59:16 -0500

    Add TSome Tests

--- src/snake.ts
+++ src/snake.ts
@@ -3,8 +3,22 @@ const start = () =>
 
   console.log('todo');
 
+const FOOD_COLORS  =  [ "red", "blue", "green" ];
+
+type Point = { x: number, y: number };
+type Food  = { location: Point, color: string };
 
 
+const random_location = function (boxPoint: Point) : Point {
+  return { x: Math.random() * boxPoint.x, y: Math.random() * boxPoint.y}
+}
 
 
-export { start };
+const create_food  =  (location: Point, color: string) => ({ location, color });
+
+
+const create_snake = (snakePos: Point, snakeColor: string) => 
+  [ create_food( snakePos, snakeColor) ];
+
+
+export { start, create_snake, create_food, random_location };
