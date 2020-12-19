4ee6f26c704bf953784e70dd44d5195cde03a31c
Christopher Miller <miller1997@gmail.com>
2020-12-19 01:35:32 -0500

    Clean Up & Hopefully Added Terser

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ const iife_config = {
   input     : 'dist/es6/snake.js',
 
   output    : {
-    file      : 'dist/es5/snake.js',
+    file      : 'dist/es5/snake.iife.js',
     format    : 'iife',
     name      : 'snake'
   }
